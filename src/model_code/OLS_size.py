c6b71fb17e110fc18535bc4f5a73128e03fffe3d
Solmaz <51123986+solmazahmadi@users.noreply.github.com>
2021-03-07 14:33:42 +0100

    Run the regression on size of firms' covariates.

---
+++ src/model_code/OLS_size.py
@@ -0,0 +1,38 @@
+import statsmodels.formula.api as sm
+
+
+def get_covariates(degree):
+    base_variables = [
+        "smallm",
+        "largem",
+        "treatsmall",
+        "treatlarge",
+        "ssmall",
+        "slarge",
+        "streatsmall",
+        "streatlarge",
+    ]
+    if degree == 0:
+        base = base_variables[0:3]
+        return base
+    if degree == 1:
+        return base_variables
+    else:
+        for i in range(2, degree + 1):
+            base_variables.append(f"ssmall{i}")
+            base_variables.append(f"slarge{i}")
+            base_variables.append(f"streatsmall{i}")
+            base_variables.append(f"streatlarge{i}")
+        return base_variables
+
+
+def regress(dataframe, degree):
+    reg = (
+        sm.ols(
+            formula="INVSALES ~ " + ("+").join(get_covariates(degree)),
+            data=dataframe,
+        )
+        .fit(cov_type="cluster", cov_kwds={"groups": dataframe["score"]}, use_t=True)
+        .summary()
+    )
+    return reg
