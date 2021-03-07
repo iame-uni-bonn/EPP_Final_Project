9537fa7662138c31fd0d5f03161ee3af2acdae6e
Solmaz <51123986+solmazahmadi@users.noreply.github.com>
2021-03-07 15:01:43 +0100

    Run the regression on covariates of age of firm's model.

---
+++ src/analysis/task_regression_on_age.py
@@ -0,0 +1,31 @@
+import json
+import pickle
+
+import pandas as pd
+import pytask
+
+from src.config import BLD
+from src.config import SRC
+from src.model_code.OLS_age import regress
+
+
+@pytask.mark.parametrize(
+    "depends_on, produces",
+    [
+        (
+            {
+                "model": SRC / "model_specs" / "age_of_firm" / f"{model_name}.json",
+                "OLS_age": SRC / "model_code" / "OLS_age.py",
+                "data": BLD / "data" / "Bronzini-Iachini_dataset.csv",
+            },
+            BLD / "analysis" / "age_of_firm" / f"regression_{model_name}.pickle",
+        )
+        for model_name in ["degree_0", "degree_1", "degree_2", "degree_3"]
+    ],
+)
+def task_regression(depends_on, produces):
+    model = json.loads(depends_on["model"].read_text(encoding="utf-8"))
+    inputs = pd.read_csv(depends_on["data"])
+    regression = regress(inputs, model["n_degree"])
+    with open(produces, "wb") as out_file:
+        pickle.dump(regression, out_file)
---
+++ src/model_code/OLS_age.py
@@ -0,0 +1,29 @@
+import statsmodels.formula.api as sm
+
+
+def get_covariates(degree):
+    base_variables = ["treatl", "treath", "s", "streatl", "streath"]
+    if degree == 0:
+        base = base_variables[0:1]
+        return base
+    if degree == 1:
+        return base_variables
+    else:
+        for i in range(2, degree + 1):
+            base_variables.append(f"s{i}")
+            base_variables.append(f"streatl{i}")
+            base_variables.append(f"streath{i}")
+
+        return base_variables
+
+
+def regress(dataframe, degree):
+    reg = (
+        sm.ols(
+            formula="INVSALES ~ " + ("+").join(get_covariates(degree)),
+            data=dataframe,
+        )
+        .fit(cov_type="cluster", cov_kwds={"groups": dataframe["score"]}, use_t=True)
+        .summary()
+    )
+    return reg
