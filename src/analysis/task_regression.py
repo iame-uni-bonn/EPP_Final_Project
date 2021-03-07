2f7ac99c80ef764ad24a77ed0796d85302fe92bd
Solmaz <51123986+solmazahmadi@users.noreply.github.com>
2021-03-07 08:07:11 +0100

    Add model specifications for different models.

--- src/analysis/task_regression.py
+++ src/analysis/task_regression.py
@@ -20,7 +20,7 @@ from src.model_code.OLS import regress
             },
             BLD / "analysis" / "baseline" / f"regression_{model_name}.pickle",
         )
-        for model_name in ["degree_0"]
+        for model_name in ["degree_0", "degree_1", "degree_2", "degree_3"]
     ],
 )
 def task_regression(depends_on, produces):
