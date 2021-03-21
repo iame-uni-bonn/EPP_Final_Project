1f1e4b74e671615055431de9b962891e39f33804
Solmaz <51123986+solmazahmadi@users.noreply.github.com>
2021-03-21 19:43:36 +0100

    created a text file

--- src/model_code/OLS_age.py
+++ src/model_code/OLS_age.py
@@ -1,29 +1,31 @@
-import statsmodels.formula.api as sm
+import econtools.metrics as mt
 
 
 def get_covariates(degree):
-    base_variables = ["treatl", "treath", "s", "streatl", "streath"]
+    base_variables = [
+        "fchighm",
+        "fclowm",
+        "treatfchigh",
+        "treatfclow",
+        "sfchigh",
+        "sfclow",
+        "streatfchigh",
+        "streatfclow",
+    ]
     if degree == 0:
-        base = base_variables[0:1]
+        base = base_variables[0:3]
         return base
     if degree == 1:
         return base_variables
     else:
         for i in range(2, degree + 1):
-            base_variables.append(f"s{i}")
-            base_variables.append(f"streatl{i}")
-            base_variables.append(f"streath{i}")
-
+            base_variables.append(f"sfchigh{i}")
+            base_variables.append(f"sfclow{i}")
+            base_variables.append(f"streatfchigh{i}")
+            base_variables.append(f"streatfclow{i}")
         return base_variables
 
 
 def regress(dataframe, degree):
-    reg = (
-        sm.ols(
-            formula="INVSALES ~ " + ("+").join(get_covariates(degree)),
-            data=dataframe,
-        )
-        .fit(cov_type="cluster", cov_kwds={"groups": dataframe["score"]}, use_t=True)
-        .summary()
-    )
+    reg = mt.reg(dataframe, "INVSALES", get_covariates(degree), cluster="score")
     return reg
--- src/model_code/OLS_coverage_ratio.py
+++ src/model_code/OLS_coverage_ratio.py
@@ -1,31 +1,29 @@
-import econtools.metrics as mt
+import statsmodels.formula.api as sm
 
 
 def get_covariates(degree):
-    base_variables = [
-        "fchighm",
-        "fclowm",
-        "treatfchigh",
-        "treatfclow",
-        "sfchigh",
-        "sfclow",
-        "streatfchigh",
-        "streatfclow",
-    ]
+    base_variables = ["treatl", "treath", "s", "streatl", "streath"]
     if degree == 0:
-        base = base_variables[0:3]
+        base = base_variables[0:2]
         return base
     if degree == 1:
         return base_variables
     else:
         for i in range(2, degree + 1):
-            base_variables.append(f"sfchigh{i}")
-            base_variables.append(f"sfclow{i}")
-            base_variables.append(f"streatfchigh{i}")
-            base_variables.append(f"streatfclow{i}")
+            base_variables.append(f"s{i}")
+            base_variables.append(f"streatl{i}")
+            base_variables.append(f"streath{i}")
+
         return base_variables
 
 
 def regress(dataframe, degree):
-    reg = mt.reg(dataframe, "INVSALES", get_covariates(degree), cluster="score")
+    reg = (
+        sm.ols(
+            formula="INVSALES ~ " + ("+").join(get_covariates(degree)),
+            data=dataframe,
+        )
+        .fit(cov_type="cluster", cov_kwds={"groups": dataframe["score"]}, use_t=True)
+        .summary()
+    )
     return reg
